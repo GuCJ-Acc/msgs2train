70e2d717e2f90a9fb9b9974543bd864008a798c8
jack-gu-nuc <1255285432@qq.com>
2025-11-08 10:39:42 +0800

    初始化msgs消息订阅处理的功能包

---
+++ src/msgs2train.cpp
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+#include <serial/serial.h>
+#include <std_msgs/String.h>
+
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <sstream>
+#include <vector>
+#include <thread>
+#include <yaml-cpp/yaml.h>
+
+#include "pressure_tr_serial/pressure_info.h"
+
+
+
+int main(int argc, char** argv)
+{
+
+
+    return 0;
+}
