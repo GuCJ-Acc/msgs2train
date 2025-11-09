6f6eefd246205874634313ce55c3752b577132f6
jack-gu-nuc <1255285432@qq.com>
2025-11-09 14:31:28 +0800

    更新训练数据的保存cpp

--- src/msgs2train.cpp
+++ src/msgs2train.cpp
@@ -13,6 +13,35 @@
 #include "pressure_tr_serial/pressure_info.h"
 
 
+class msgs2train
+{
+public:
+    std::string Topic_MotorState;
+    std::string Topic_PressureValues;
+
+    std::mutex mtx_buff;
+
+    int Value_RF;
+    int Value_RH;
+    int Value_LF;
+    int Value_LH;
+
+    ros::NodeHandle nh;
+    ros::Subscriber sub_MotorState;
+    ros::Subscriber sub_PressureValues;
+
+    msgs2train()
+    {
+        YAML::Node sub_yaml;
+        sub_yaml = YAML::LoadFile(std::string(std::string(ROOT_DIR)) + "config/pressureSerialPort.yaml");
+        Topic_MotorState = sub_yaml["Topic_MotorState"].as<std::string>();
+        Topic_PressureValues = sub_yaml["Topic_PressureValues"].as<std::string>();
+
+    }
+
+    ~msgs2train() {}
+};
+
 
 int main(int argc, char** argv)
 {
