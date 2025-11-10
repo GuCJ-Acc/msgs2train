b408062ea0b5051081f1328b4c1c8085681e77b5
jack-gu-nuc <1255285432@qq.com>
2025-11-10 20:25:47 +0800

    新增相关msg回调函数

--- src/msgs2train.cpp
+++ src/msgs2train.cpp
@@ -1,14 +1,17 @@
 #include <ros/ros.h>
 #include <serial/serial.h>
 #include <std_msgs/String.h>
+#include <sensor_msgs/JointState.h>
 
 #include <iostream>
 #include <string>
+#include <deque>
 #include <mutex>
 #include <sstream>
 #include <vector>
 #include <thread>
 #include <yaml-cpp/yaml.h>
+#include <fstream>
 
 #include "pressure_tr_serial/pressure_info.h"
 
@@ -30,16 +33,79 @@ public:
     ros::Subscriber sub_MotorState;
     ros::Subscriber sub_PressureValues;
 
+    bool init_flag_MotorState = false;
+    bool init_flag_PressureValue = false;
+
+    std::deque<sensor_msgs::JointState::ConstPtr> buff_MotorState;
+    std::deque<pressure_tr_serial::pressure_info::ConstPtr> buff_PressureValue;
+    std::deque<double> buff_time_PressureValue;
+    
+
     msgs2train()
     {
         YAML::Node sub_yaml;
-        sub_yaml = YAML::LoadFile(std::string(std::string(ROOT_DIR)) + "config/pressureSerialPort.yaml");
+        sub_yaml = YAML::LoadFile(std::string(std::string(ROOT_DIR)) + "config/msgs2train.yaml");
         Topic_MotorState = sub_yaml["Topic_MotorState"].as<std::string>();
         Topic_PressureValues = sub_yaml["Topic_PressureValues"].as<std::string>();
 
+        sub_MotorState = nh.subscribe<sensor_msgs::JointState>(Topic_MotorState, 1000, &msgs2train::Topic_MotorState_CallBack, this, ros::TransportHints().tcpNoDelay());
+        sub_PressureValues = nh.subscribe<pressure_tr_serial::pressure_info>(Topic_PressureValues, 1000, &msgs2train::Topic_PressureValues_CallBack, this, ros::TransportHints().tcpNoDelay());
+    
     }
 
     ~msgs2train() {}
+
+    void Topic_MotorState_CallBack(const sensor_msgs::JointState::ConstPtr& msg)
+    {
+        mtx_buff.lock();
+
+        if (init_flag_MotorState == false){
+            init_flag_MotorState = true;
+        }
+
+        buff_MotorState.push_back(msg);
+
+        mtx_buff.unlock();
+    }
+
+    void Topic_PressureValues_CallBack(const pressure_tr_serial::pressure_info::ConstPtr& msg)
+    {
+        mtx_buff.lock();
+
+        if (init_flag_PressureValue == false) {
+            init_flag_PressureValue = true;
+        }
+
+        buff_PressureValue.push_back(msg);
+
+        mtx_buff.unlock();
+    }
+
+    void sync_DataProcess()
+    {
+        if (buff_MotorState.empty() || buff_PressureValue.empty()) {
+            return;
+        }
+
+
+        double time_PressureValue = buff_PressureValue.front()->header.stamp.toSec();
+        double time_MotorState = buff_MotorState.front()->header.stamp.toSec();
+        while ((!buff_MotorState.empty()) && (time_MotorState < time_PressureValue))
+        {
+            time_MotorState = buff_MotorState.front()->header.stamp.toSec();
+            if (time_MotorState > time_PressureValue)
+                break;
+
+            // save MotorState data to file for MLP train
+
+
+            buff_MotorState.pop_front();
+        }
+
+        buff_PressureValue.pop_front();
+
+    }
+
 };
 
 
